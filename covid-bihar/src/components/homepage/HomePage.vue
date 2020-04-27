880da502a11fff0eb9c1278285caa2a27d6fb43f
shuvikash <shuvikash.skp@gmail.com>
2020-04-28 00:05:10 +0530

    added better structure for the card

--- covid-bihar/src/components/homepage/HomePage.vue
+++ covid-bihar/src/components/homepage/HomePage.vue
@@ -1,7 +1,18 @@
 <template>
     <div class="homepage">
-        <v-container fluid>
-            <main-card></main-card>
+        <v-container>
+            <v-row>
+                <v-col cols="12" sm="9" md="9">
+                    <v-row>
+                        <v-col sm="4">
+                            <main-card></main-card>
+                        </v-col>
+                    </v-row>
+                </v-col>
+                <v-col cols="12" sm="3" md="3">
+
+                </v-col>
+            </v-row>
             <table-card></table-card>
         </v-container>
     </div>
--- covid-bihar/src/components/homepage/components/MainCard.vue
+++ covid-bihar/src/components/homepage/components/MainCard.vue
@@ -1,28 +1,7 @@
 <template>
-    <v-row>
-        <v-col cols="12" sm="9" md="9">
-            <v-row>
-                <v-col sm="4">
-                    <v-card class="mx-auto" raised>
-                        <v-card-title>BIHAR</v-card-title>
-                    </v-card>
-                </v-col>
-                <v-col sm="4">
-                    <v-card class="mx-auto" raised>
-                        <v-card-title>INDIA</v-card-title>
-                    </v-card>
-                </v-col>
-                <v-col sm="4">
-                    <v-card class="mx-auto" raised>
-                        <v-card-title>WORLD</v-card-title>
-                    </v-card>
-                </v-col>
-            </v-row>
-        </v-col>
-        <v-col cols="12" sm="3" md="3">
-
-        </v-col>
-    </v-row>
+    <v-card class="mx-auto" raised>
+        <v-card-title>BIHAR</v-card-title>
+    </v-card>
 </template>
 <script>
 export default {
