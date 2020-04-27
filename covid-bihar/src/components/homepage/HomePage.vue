65d80790afcff766a7101e56d97452d3d157fb14
sengarsameer <sengar.sameer@gmail.com>
2020-04-27 23:17:41 +0530

    first commit

---
+++ covid-bihar/src/components/homepage/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="homepage">
+        <table-card />
+    </div>
+</template>
+
+<script>
+import TableCard from './components/TableCard'
+export default {
+    name: 'HomePage',
+    components: {
+        TableCard
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
