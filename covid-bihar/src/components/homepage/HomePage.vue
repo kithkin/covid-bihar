d8a3fce7715b3f8bcbeaf5c8d035892eeeff6a0b
shuvikash <shuvikash.skp@gmail.com>
2020-04-27 23:22:01 +0530

    added main card component with home page component

---
+++ covid-bihar/src/components/homepage/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <MainCard></MainCard>
+  </div>
+</template>
+
+<script>
+import MainCard from './components/MainCard.vue'
+
+export default {
+  name: 'HomePage',
+  components: {
+    MainCard
+  }
+}
+</script>
---
+++ covid-bihar/src/components/homepage/components/MainCard.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+    hello
+    </div>
+</template>
+<script>
+export default {
+  name: 'MainCard',
+  data() {
+      return {
+
+      }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
