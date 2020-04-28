793372ea76c299aa954dd5b7dff3f492ae9496c9
sengarsameer <sengar.sameer@gmail.com>
2020-04-28 23:15:20 +0530

    first commit

--- covid-bihar/src/components/homepage/components/TableCard.vue
+++ covid-bihar/src/components/homepage/components/TableCard.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
-        <v-data-table
-        :headers="headers"
-        :items="desserts"
-        :options.sync="options"
-        :server-items-length="totalDesserts"
-        :loading="loading"
-        class="elevation-1"
-        ></v-data-table>
+        <v-card class="mx-auto" raised>
+        <table class="table table-bordered table-hover">
+            <thead>
+                <tr> 
+                    <th class="th-sm">DISTRICT&nbsp;<i ></i></th>
+                    <th class="th-sm">TOTAL&nbsp;<i ></i></th>
+                    <th class="th-sm">CONFIRMED&nbsp;<i ></i></th>
+                    <th class="th-sm">NEW CASES&nbsp;<i ></i></th>
+                    <th class="th-sm">DEATH&nbsp;<i ></i></th>
+                    <th class="th-sm">NEW DEATH&nbsp;<i ></i></th>
+                    <th class="th-sm">RECOVERED&nbsp;<i ></i></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td >HELLO-1</td>
+                    <td >HELLO-2</td>
+                    <td >HELLO-3</td>
+                    <td class="covid-bg-yellow">HELLO-4</td>
+                    <td >HELLO-5</td>
+                    <td class="covid-bg-red">HELLO-6</td>
+                    <td >HELLO-7</td>
+                </tr>                                                
+            </tbody>
+        </table>
+        </v-card>
     </div>
 </template>
 
@@ -16,78 +34,19 @@ export default {
     name: 'tablecard',
     data () {
         return {
-            headers: [
-                {
-                    text: 'Dessert (100g serving)',
-                    align: 'start',
-                    sortable: false,
-                    value: 'name',
-                },
-                { text: 'Calories', value: 'calories' },
-                { text: 'Fat (g)', value: 'fat' },
-                { text: 'Carbs (g)', value: 'carbs' },
-                { text: 'Protein (g)', value: 'protein' },
-                { text: 'Iron (%)', value: 'iron' },
-            ],
-            desserts: [
-                {
-                    name: 'Frozen Yogurt',
-                    calories: 159,
-                    fat: 6.0,
-                    carbs: 24,
-                    protein: 4.0,
-                    iron: '1%',
-                },
-                {
-                    name: 'Ice cream sandwich',
-                    calories: 237,
-                    fat: 9.0,
-                    carbs: 37,
-                    protein: 4.3,
-                    iron: '1%',
-                },
-                {
-                    name: 'Eclair',
-                    calories: 262,
-                    fat: 16.0,
-                    carbs: 23,
-                    protein: 6.0,
-                    iron: '7%',
-                },
-                {
-                    name: 'Cupcake',
-                    calories: 305,
-                    fat: 3.7,
-                    carbs: 67,
-                    protein: 4.3,
-                    iron: '8%',
-                },
-                {
-                    name: 'Gingerbread',
-                    calories: 356,
-                    fat: 16.0,
-                    carbs: 49,
-                    protein: 3.9,
-                    iron: '16%',
-                },
-                {
-                    name: 'Jelly bean',
-                    calories: 375,
-                    fat: 0.0,
-                    carbs: 94,
-                    protein: 0.0,
-                    iron: '0%',
-                },
-                {
-                    name: 'Lollipop',
-                    calories: 392,
-                    fat: 0.2,
-                    carbs: 98,
-                    protein: 0,
-                    iron: '2%',
-                }
-            ],
+
         }
     },
 }
 </script>
+
+<style>
+    .covid-bg-red {
+        background-color: red;
+        color: white;
+    }
+    .covid-bg-yellow {
+        background-color: #FFEEAA;
+    }
+
+</style>
