8d42524bff71fa223ea596007ac7ef027b53081a
shuvikash <shuvikash.skp@gmail.com>
2020-04-29 01:19:17 +0530

    data fetching logic updated

--- covid-bihar/src/services/index.js
+++ covid-bihar/src/services/index.js
@@ -6,27 +6,25 @@ Vue.use(axios);
 const getAllData = () => new Promise ((resolve, reject) => {
     axios.get('https://docs.google.com/spreadsheets/d/1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ/export?format=csv&id=1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ')
     .then(res => {
-        // const toJSON = csv => {
-        //     const lines = csv.split('\n')
-        //     const result = []
-        //     const headers = lines[0].split(',')
-        
-        //     lines.map(l => {
-        //         const obj = {}
-        //         const line = l.split(',')
-        
-        //         headers.map((h, i) => {
-        //             obj[h] = line[i]
-        //         })
-        
-        //         result.push(obj)
-        //     })
-        
-        //     return JSON.stringify(result)
-        // }
-        // const csv = res.data;
-        // const jsonData = toJSON(csv)
-        resolve(res.data);
+        const toJSON = csv => {
+            const lines = csv.split('\n')
+            const result = []
+            const headers = lines[0].split(',')
+            for(var i=1;i<lines.length;i++){
+                var obj = {};
+                var currentline=lines[i].split(",");
+          
+                for(var j=0;j<headers.length;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+          
+                result.push(obj);
+            }
+            return JSON.stringify(result);
+        }
+        const csv = res.data;
+        const jsonData = toJSON(csv);
+        resolve(jsonData);
     })
     .catch(err => {
         reject(err);
