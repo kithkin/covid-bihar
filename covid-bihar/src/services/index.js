864f670511112260f34e9050b7bd154bd5e7171d
sengarsameer <sengar.sameer@gmail.com>
2020-04-29 16:58:48 +0530

    first commit

--- covid-bihar/src/services/index.js
+++ covid-bihar/src/services/index.js
@@ -1,11 +1,8 @@
-import Vue from 'vue';
 import axios from 'axios';
 
-Vue.use(axios);
-
-const getAllData = () => new Promise ((resolve, reject) => {
-    axios.get('https://docs.google.com/spreadsheets/d/1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ/export?format=csv&id=1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ')
-    .then(res => {
+const getAllData = async() => {
+    try {
+        const resp = await axios.get('https://docs.google.com/spreadsheets/d/1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ/export?format=csv&id=1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ')
         const toJSON = csv => {
             const lines = csv.split('\n')
             const result = []
@@ -20,17 +17,108 @@ const getAllData = () => new Promise ((resolve, reject) => {
           
                 result.push(obj);
             }
-            return JSON.stringify(result);
+            return result;
         }
-        const csv = res.data;
+        const csv = resp.data;
         const jsonData = toJSON(csv);
-        resolve(jsonData);
-    })
-    .catch(err => {
-        reject(err);
-    })
-})
+        return jsonData
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const getDistrictData = async() => {
+    
+}
+
+const getRecovered = async() => {
+    try {
+        const resp = await axios.get('https://docs.google.com/spreadsheets/d/1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ/export?format=csv&id=1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ&gid=432629340')
+        const toJSON = csv => {
+            const lines = csv.split('\n')
+            const result = []
+            const headers = lines[0].split(',')
+            for(var i=1;i<lines.length;i++){
+                var obj = {};
+                var currentline=lines[i].split(",");
+          
+                for(var j=0;j<headers.length;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+          
+                result.push(obj);
+            }
+            return result;
+        }
+        const csv = resp.data;
+        const jsonData = toJSON(csv);
+        return jsonData
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const getTotal = async() => {
+    try {
+        const resp = await axios.get('https://docs.google.com/spreadsheets/d/1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ/export?format=csv&id=1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ&gid=600675561')
+        const toJSON = csv => {
+            const lines = csv.split('\n')
+            const result = []
+            const headers = lines[0].split(',')
+            for(var i=1;i<lines.length;i++){
+                var obj = {};
+                var currentline=lines[i].split(",");
+          
+                for(var j=0;j<headers.length;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+          
+                result.push(obj);
+            }
+            return result;
+        }
+        const csv = resp.data;
+        const jsonData = toJSON(csv);
+        return jsonData
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+const getDeath = async() => {
+    try {
+        const resp = await axios.get('https://docs.google.com/spreadsheets/d/1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ/export?format=csv&id=1gw1m2JuspwZ7a8SEijkw-M-3g377GGzXdAYKehzCpHQ&gid=1324261680')
+        const toJSON = csv => {
+            const lines = csv.split('\n')
+            const result = []
+            const headers = lines[0].split(',')
+            for(var i=1;i<lines.length;i++){
+                var obj = {};
+                var currentline=lines[i].split(",");
+          
+                for(var j=0;j<headers.length;j++){
+                    obj[headers[j]] = currentline[j];
+                }
+          
+                result.push(obj);
+            }
+            return result;
+        }
+        const csv = resp.data;
+        const jsonData = toJSON(csv);
+        return jsonData
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
 
 export default {
-    getAllData
+    getAllData,
+    getTotal,
+    getRecovered,
+    getDeath
 }
