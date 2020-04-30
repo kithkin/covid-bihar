f6ed53f850723d294303e47d05d60d1e8717cdf6
Ronij Pandey <ronij.2508@gmail.com>
2020-04-30 18:38:20 +0530

    Create map.vue

---
+++ covid-bihar/src/components/mapbuild/map.vue
@@ -0,0 +1,72 @@
+<!--
+
+It would be great if you color the map by population using data from index.vue
+
+Based on:
+  http://bl.ocks.org/rveciana/a2a1c21ca1c71cd3ec116cc911e5fce9
+  http://bl.ocks.org/mapsam/6083585
+
+
+Links:
+
+-->
+
+<template>
+  <svg width="500" height="300"></svg>
+</template>
+
+<script>
+
+const d3 = require('d3');
+const topojson = require('topojson');
+
+const json = require('./static/data/bihar.json');
+
+export default {
+  mounted: function() {
+    var v = this;
+    var svg = d3.select(this.$el);
+    var width = +svg.attr('width');
+    var height = +svg.attr('height');
+
+    width = width*height;
+    height = width*height;
+
+    var projection = d3.geoMercator();
+    console.log(height);
+    var path = d3.geoPath().projection(projection);
+
+    var func = function(us) {
+      var g = svg.append('g');
+        g
+          .selectAll('.state')
+          .data(topojson.feature(us, us.objects.Bihar).features)
+          .enter()
+          .append("path")
+          .attr("class", "state")
+          .attr("d", path)
+          .on('mouseover', function(d) {
+            v.$emit('stateSelected', d.properties.Dist_Name)
+              })
+          .on('mouseout', function(d) {
+            v.$emit('stateDeselected', d.properties.Dist_Name)
+          })
+      g.attr('transform', 'scale(1.4)')
+    };
+
+    func(json);
+
+  }
+  // TODO: fire events
+}
+</script>
+
+<style>
+.state {
+  fill: #ccc;
+  stroke: #fff;
+}
+.state:hover {
+  fill: steelblue;
+}
+</style>
