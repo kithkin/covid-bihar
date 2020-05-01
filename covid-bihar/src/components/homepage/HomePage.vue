8d57d72091531ecfaf2ba33cf4d6ceb9cf2b0d76
shuvikash <shuvikash.skp@gmail.com>
2020-05-01 15:52:31 +0530

    mobile view fixed

--- covid-bihar/src/components/homepage/HomePage.vue
+++ covid-bihar/src/components/homepage/HomePage.vue
@@ -29,8 +29,10 @@
                         <v-container
                         class="fill-height"
                         >
-                            <v-row>
-                                <v-col cols="12" sm="9" md="9">
+                            <v-row 
+                            align="center"
+                            justify="center">
+                                <v-col cols="12">
                                     <v-row>
                                         <v-col sm="4" v-for="(data,index) in covidData" :key="'data_' + data + `${index}`">
                                             <main-card :covid-data="data"></main-card>
@@ -58,9 +60,6 @@
                                             <table-card :districtData="districtData"></table-card>
                                         </v-col>
                                     </v-row>
-                                </v-col>
-                                <v-col cols="12" sm="3" md="3">
-                                    
                                 </v-col>
                             </v-row>
                         </v-container>
