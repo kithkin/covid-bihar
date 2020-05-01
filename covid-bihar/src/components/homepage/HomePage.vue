8e4238a2cafb4e2d66d6779da255240763e54b68
sengarsameer <sengar.sameer@gmail.com>
2020-05-01 22:17:13 +0530

    first commit

--- covid-bihar/src/components/homepage/HomePage.vue
+++ covid-bihar/src/components/homepage/HomePage.vue
@@ -38,7 +38,7 @@
                                             <main-card :covid-data="data"></main-card>
                                         </v-col>
                                     </v-row>
-                                    <v-row>
+                                    <!-- <v-row>
                                         <v-col sm="12" class="text-center">
                                              <div id="holder">
                                                 <div class="mapHolder">
@@ -54,7 +54,7 @@
                                                     />
                                             </div>
                                         </v-col>
-                                    </v-row>
+                                    </v-row> -->
                                     <v-row>
                                         <v-col sm="12">
                                             <table-card :districtData="districtData"></table-card>
@@ -73,16 +73,16 @@
 import MainCard from './components/MainCard.vue';
 import TableCard from './components/TableCard.vue';
 import serviceData from './../../services/index.js';
-const map = require('./../mapbuild/map').default;
-const tooltip = require('./../mapbuild/tooltip').default;
+// const map = require('./../mapbuild/map').default;
+// const tooltip = require('./../mapbuild/tooltip').default;
 
 export default {
     name: 'HomePage',
     components: {
         TableCard,
         MainCard,
-        usMap: map,
-        tooltip: tooltip
+        // usMap: map,
+        // tooltip: tooltip
     },
     data() {
         return {
