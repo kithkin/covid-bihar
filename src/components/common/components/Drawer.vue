660952a283f2129c4bedc5ac329c28bf7c1dad73
shuvikash <shuvikash.skp@gmail.com>
2020-05-03 00:41:09 +0530

    drawer fixed

--- src/components/common/components/Drawer.vue
+++ src/components/common/components/Drawer.vue
@@ -2,8 +2,24 @@
     <v-navigation-drawer
     v-model="model"
     clipped="clipped"
+    width="140"
     app
     >
+        <v-list dense>
+            <v-list-item
+            v-for="item in items"
+            :key="item.title"
+            link
+            >
+                <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                </v-list-item-icon>
+
+                <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
     </v-navigation-drawer>
 </template>
 
@@ -11,5 +27,12 @@
   export default {
     name: 'Drawer',
     props: ['model', 'clipped'],
+    data() {
+        return {
+            items: [
+                { icon: 'mdi-help-circle', title: 'About' } 
+            ]
+        }
+    }
   }
 </script>
