42a33632819d0a130b026b2c390550b0bd2e0a40
shuvikash <shuvikash.skp@gmail.com>
2020-05-02 23:29:30 +0530

    drawer fixed

--- src/components/common/components/AppBar.vue
+++ src/components/common/components/AppBar.vue
@@ -1,31 +1,39 @@
 <template>
-    <v-app-bar
-    :clipped-left="primaryDrawer.clipped"
-    color="blue lighten-2"
-    dark
-    app
-    >
-        <v-app-bar-nav-icon
-        @click.stop="primaryDrawer.model = !primaryDrawer.model"
-        class="ml-1"
-        />
-        <div class="pt-4">
-            <v-img :src="require('@/assets/logo.png')" height="60" width= "190"/>
-        </div>  
-        <v-spacer />
-        <v-switch
-        v-model="$vuetify.theme.dark"
-        class="mt-5"
-        />
-        <span>
-            Dark
-        </span>
-    </v-app-bar>
+    <div>
+        <drawer :clipped="primaryDrawer.clipped" :model="primaryDrawer.model"></drawer>
+        <v-app-bar
+        :clipped-left="primaryDrawer.clipped"
+        color="blue lighten-2"
+        dark
+        app
+        >
+            <v-app-bar-nav-icon
+            @click.stop="primaryDrawer.model = !primaryDrawer.model"
+            class="ml-1"
+            />
+            <div class="pt-4">
+                <v-img :src="require('@/assets/logo.png')" height="60" width= "190"/>
+            </div>  
+            <v-spacer />
+            <v-switch
+            v-model="$vuetify.theme.dark"
+            class="mt-5"
+            />
+            <span>
+                Dark
+            </span>
+        </v-app-bar>
+    </div>
 </template>
 
 <script>
+  import Drawer from './Drawer.vue'
+
   export default {
     name: 'AppBar',
+    components: {
+        Drawer
+    },
     data() {
         return {
             primaryDrawer: {
--- src/components/homepage/HomePage.vue
+++ src/components/homepage/HomePage.vue
@@ -2,7 +2,7 @@
     <div class="homepage">
         <v-app id="covid">
             <!-- drawer component -->
-            <drawer></drawer>
+            <!-- <drawer></drawer> -->
 
             <!-- app bar component -->
             <app-bar></app-bar>
@@ -78,7 +78,7 @@
 
 <script>
 import AppBar from './../common/components/AppBar.vue';
-import Drawer from './../common/components/Drawer.vue';
+// import Drawer from './../common/components/Drawer.vue';
 import MainCard from './components/MainCard.vue';
 import TableCard from './components/TableCard.vue';
 import serviceData from './../../services/index.js';
@@ -91,7 +91,7 @@ export default {
     name: 'HomePage',
     components: {
         AppBar,
-        Drawer,
+        // Drawer,
         TableCard,
         MainCard,
         usMap: map,
