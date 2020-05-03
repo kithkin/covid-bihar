a1f4df5e216a0aeda674352bba469e087bf64277
sengarsameer <sengar.sameer@gmail.com>
2020-05-03 23:23:29 +0530

    fix folder name

---
+++ src/components/commonpage/components/AppBar.vue
@@ -0,0 +1,92 @@
+<template>
+    <div>
+        <v-navigation-drawer
+        v-model="primaryDrawer.model"
+        :clipped="primaryDrawer.clipped"
+        width="90"
+        app
+        >
+            <v-list 
+            dense
+            nav
+            >
+                <div class="pl-3">
+                    <v-list-item-avatar>
+                    <img src="./../../../assets/favicon.png">
+                    </v-list-item-avatar>
+                </div>
+
+                <v-divider></v-divider>
+                
+                <v-list-item
+                v-for="item in items"
+                :key="item.title"
+                link
+                >
+                    <router-link :to="item.link" style="text-decoration: none;">
+                    
+                    <v-list-item-icon :class="item.iconClass">
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+
+                    <v-list-item-content>
+                        <v-list-item-title :class="item.textClass">{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+
+                    </router-link>
+                   
+
+                </v-list-item>
+                
+            </v-list>
+        </v-navigation-drawer>
+        <v-app-bar
+        :clipped-left="primaryDrawer.clipped"
+        color="blue lighten-2"
+        dark
+        app
+        >
+            <v-app-bar-nav-icon
+            @click.stop="primaryDrawer.model = !primaryDrawer.model"
+            class="ml-1"
+            />
+            <div class="pt-2">
+                <router-link to="/covid-bihar">
+                <v-img :src="require('@/assets/logoHi.png')" height="136" width= "136"/>
+                </router-link>
+            </div>  
+            <v-spacer />
+            <v-switch
+            v-model="$vuetify.theme.dark"
+            class="mt-5"
+            />
+            <span>
+                डार्क मोड
+            </span>
+        </v-app-bar>
+    </div>
+</template>
+
+<script>
+//   import Drawer from './Drawer.vue'
+
+  export default {
+    name: 'AppBar',
+    components: {
+        // Drawer
+    },
+    data() {
+        return {
+            primaryDrawer: {
+                model: false,
+                type: 'default (no property)',
+                clipped: true,
+            },
+            items: [
+                { icon: 'mdi-home-circle', iconClass: 'pl-4', title: 'मुख्य पृष्ठ', link: '/covid-bihar', textClass: 'font-weight-bold pl-1' },
+                { icon: 'mdi-help-circle', iconClass: 'pl-4', title: 'हमारे बारे में', link: '/covid-bihar/about', textClass: 'font-weight-bold' },
+            ]
+        }
+    },
+  }
+</script>
---
+++ src/components/commonpage/components/FooterCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-footer
+  padless
+  >
+      <v-row
+      >
+        <v-col
+        class="blue lighten-2 py-4 text-center white--text"
+        cols="12"
+        >
+          <strong>कोविड-बिहार</strong>
+          <div class="body-1 font-weight-light pt-2 text-center">
+              &copy; {{ new Date().getFullYear() }}, 
+              <v-icon size="20" color='red darken-2'>
+                mdi-heart
+              </v-icon>
+              बिहारियों के लिए
+          </div>
+        </v-col>
+      </v-row>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'FooterCard',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
