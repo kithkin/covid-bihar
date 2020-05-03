f447fde83e4ac061bf6c46ba9dd8f2a9d38567bc
shuvikash <shuvikash.skp@gmail.com>
2020-05-03 18:22:06 +0530

    components structure modularised

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,59 @@
 <template>
   <div id="app">
-    <router-view />
+      <v-app id="covid">
+
+        <!-- app bar component -->
+        <app-bar></app-bar>
+
+        <!-- back to top component -->
+        <v-btn
+          v-scroll="onScroll"
+          v-show="fab"
+          fab
+          dark
+          fixed
+          bottom
+          right
+          color="primary"
+          @click="toTop"
+        >
+          <v-icon>keyboard_arrow_up</v-icon>
+        </v-btn>
+
+        <!-- router view -->
+        <router-view />
+
+        <!-- footer component -->
+        <footer-card />
+      </v-app>
   </div>
 </template>
 
 <script>
+import AppBar from './components/common/components/AppBar.vue';
+import FooterCard from './components/common/components/FooterCard.vue';
 
 export default {
   name: 'App',
+  components: {
+      AppBar,
+      FooterCard
+  },
+  data() {
+      return {
+        fab: false,
+      }
+  },
+  methods: {
+    onScroll (e) {
+      if (typeof window === 'undefined') return
+          const top = window.pageYOffset ||   e.target.scrollTop || 0
+          this.fab = top > 20
+    },
+    toTop () {
+      this.$vuetify.goTo(0);
+    }
+  },
   watch: {
     // eslint-disable-next-line no-unused-vars
     '$route' (to, from) {
--- src/components/homepage/HomePage.vue
+++ src/components/homepage/HomePage.vue
@@ -1,88 +1,60 @@
 <template>
     <div class="homepage">
-        <v-app id="covid">
-            <!-- drawer component -->
-            <!-- <drawer></drawer> -->
-
-            <!-- app bar component -->
-            <app-bar></app-bar>
-
-            <!-- back to top component -->
-            <v-btn
-                v-scroll="onScroll"
-                v-show="fab"
-                fab
-                dark
-                fixed
-                bottom
-                right
-                color="primary"
-                @click="toTop"
+        <!-- homepage content -->
+        <v-content>
+            <v-container
+            class="fill-height"
             >
-                <v-icon>keyboard_arrow_up</v-icon>
-            </v-btn>
-            <!-- homepage content -->
-            <v-content>
-                <v-container
-                class="fill-height"
-                >
-                    <v-row 
-                    align="center"
-                    justify="center">
-                        <v-col sm="12" class="pb-0 pt-2">
-                            <div class="text-right body-2">अंतिम अपडेट: {{utime}}</div>
-                        </v-col>
-                        <v-col cols="12">
-                            <v-row>
-                                <v-col sm="4" v-for="(data,index) in covidData" :key="'data_' + data + `${index}`">
-                                    <main-card :covid-data="data"></main-card>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col sm="12" class="text-center">
-                                        <div id="holder">
-                                        <div class="mapHolder">
-                                            <tooltip
-                                                v-if="currentState"
-                                                :title="currentState.districtHi"
-                                                :description="currentStateDescription"
-                                            />
-                                            <us-map
-                                                v-on:stateSelected="onStateSelected"
-                                                v-on:stateDeselected="onStateDeselected"
-                                            />
-                                        </div>
+                <v-row 
+                align="center"
+                justify="center">
+                    <v-col sm="12" class="pb-0 pt-2">
+                        <div class="text-right body-2">अंतिम अपडेट: {{utime}}</div>
+                    </v-col>
+                    <v-col cols="12">
+                        <v-row>
+                            <v-col sm="4" v-for="(data,index) in covidData" :key="'data_' + data + `${index}`">
+                                <main-card :covid-data="data"></main-card>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="12" class="text-center">
+                                    <div id="holder">
+                                    <div class="mapHolder">
+                                        <tooltip
+                                            v-if="currentState"
+                                            :title="currentState.districtHi"
+                                            :description="currentStateDescription"
+                                        />
+                                        <us-map
+                                            v-on:stateSelected="onStateSelected"
+                                            v-on:stateDeselected="onStateDeselected"
+                                        />
                                     </div>
-                                </v-col>
-                            </v-row>
-                            <v-row>
-                                <v-col sm="12">
-                                    <table-card :districtData="districtData" :loadingTable="loadingTable" :indiaData="indiaData" :worldData="worldData"></table-card>
-                                </v-col>
-                            </v-row>
-                            <!-- <v-row>
-                                <v-col>
-                                    <daily-basis-data-chart />
-                                </v-col>
-                            </v-row> -->
-                        </v-col>
-                    </v-row>
-                </v-container>
-            </v-content>
-
-            <!-- footer component -->
-            <footer-card />
-        </v-app>
+                                </div>
+                            </v-col>
+                        </v-row>
+                        <v-row>
+                            <v-col sm="12">
+                                <table-card :districtData="districtData" :loadingTable="loadingTable" :indiaData="indiaData" :worldData="worldData"></table-card>
+                            </v-col>
+                        </v-row>
+                        <!-- <v-row>
+                            <v-col>
+                                <daily-basis-data-chart />
+                            </v-col>
+                        </v-row> -->
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
     </div>
 </template>
 
 <script>
-import AppBar from './../common/components/AppBar.vue';
-// import Drawer from './../common/components/Drawer.vue';
 import MainCard from './components/MainCard.vue';
 import TableCard from './components/TableCard.vue';
 import serviceData from './../../services/index.js';
-import FooterCard from './../common/components/FooterCard.vue';
 // import DailyBasisDataChart from './components/DailyBasisDataChart.vue';
 const map = require('./../mapbuild/map').default;
 const tooltip = require('./../mapbuild/tooltip').default;
@@ -90,22 +62,14 @@ const tooltip = require('./../mapbuild/tooltip').default;
 export default {
     name: 'HomePage',
     components: {
-        AppBar,
-        // Drawer,
         TableCard,
         MainCard,
         usMap: map,
         tooltip: tooltip,
-        FooterCard,
         // DailyBasisDataChart
     },
     data() {
         return {
-            primaryDrawer: {
-                model: false,
-                type: 'default (no property)',
-                clipped: true,
-            },
             covidData: [],
             statesData: undefined,
             currentState: undefined,
@@ -113,7 +77,6 @@ export default {
             indiaData: [],
             worldData: [],
             loadingTable: true,
-            fab: false,
             utime: '',
         }
     },
@@ -169,14 +132,6 @@ export default {
         onStateDeselected: function(stateCode) {
         this.currentState = stateCode;
         this.currentState = undefined;
-        },
-        onScroll (e) {
-            if (typeof window === 'undefined') return
-            const top = window.pageYOffset ||   e.target.scrollTop || 0
-            this.fab = top > 20
-        },
-        toTop () {
-            this.$vuetify.goTo(0);
         }
     }
 }
