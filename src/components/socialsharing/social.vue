c9853595f80a80e6a3f9bb847bacfb6a13148b36
sengarsameer <sengar.sameer@gmail.com>
2020-05-06 02:19:46 +0530

    minor fixes

--- src/components/socialsharing/social.vue
+++ src/components/socialsharing/social.vue
@@ -1,11 +1,13 @@
 <template>
-    <social-sharing url="https://hamaarbihar.live/covid-bihar/"
-                    title="कोविद-बिहार | बिहार का कोरोनावायरस ट्रेंडिंग डेटा"
-                    description="बिहार के सभी जिले के कोरोनवायरस ट्रेंडिंग डेटा का पता लगाएं"
-                    quote="बिहार के सभी जिले के कोरोनवायरस ट्रेंडिंग डेटा का पता लगाएं"
-                    hashtags="covid19,bihar"
-                    twitter-user="hamaarbihar"
-                    inline-template>
+    <social-sharing 
+        url="https://hamaarbihar.live/covid-bihar/"
+        title="कोविद-बिहार | बिहार का कोरोनावायरस ट्रेंडिंग डेटा"
+        description="बिहार के सभी जिले के कोरोनवायरस ट्रेंडिंग डेटा का पता लगाएं"
+        quote="बिहार के सभी जिले के कोरोनवायरस ट्रेंडिंग डेटा का पता लगाएं"
+        hashtags="covid19,bihar"
+        twitter-user="hamaarbihar"
+        inline-template
+    >
         <div>
             <network network="facebook" class="mx-4 blue--text">
                 <i class="fab fa-facebook-square fa-2x"></i>
@@ -20,6 +22,7 @@
                 <i class="fab fa-whatsapp-square fa-2x"></i>
             </network>
         </div>
+        
     </social-sharing>
 </template>
 
