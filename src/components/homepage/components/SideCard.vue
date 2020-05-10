1fafc4ed42816a0bc8fc93dc9affdf2cee8eb87e
shuvikash <shuvikash.skp@gmail.com>
2020-05-10 23:26:50 +0530

    side card added

---
+++ src/components/homepage/components/SideCard.vue
@@ -0,0 +1,135 @@
+<template>
+    <v-card
+    class="mx-auto"
+    raised
+  >
+    <v-sheet class="pa-4 primary blue lighten 2">
+        <v-autocomplete
+        v-model="select"
+        :loading="loading"
+        :items="items"
+        :search-input.sync="search"
+        cache-items
+        dark
+        flat
+        hide-no-data
+        hide-details
+        clearable
+        clear-icon="mdi-close-circle-outline"
+        label="e.g: Patna"
+        solo-inverted
+        >
+        </v-autocomplete>
+    </v-sheet>
+    <v-card-text 
+      id="scroll-target"
+      style="max-height: 400px"
+      class="overflow-y-auto"
+    >
+      <v-row
+        v-scroll:#scroll-target="onScroll"
+        align="center"
+        justify="center"
+        style="height: 1000px"
+      >
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+
+export default {
+    name: 'SideCard',
+
+    data() {
+        return {
+            offsetTop: 0,
+            loading: false,
+            items: [],
+            search: null,
+            select: null,
+            districts: [
+            'Alabama',
+            'Alaska',
+            'American Samoa',
+            'Arizona',
+            'Arkansas',
+            'California',
+            'Colorado',
+            'Connecticut',
+            'Delaware',
+            'District of Columbia',
+            'Federated States of Micronesia',
+            'Florida',
+            'Georgia',
+            'Guam',
+            'Hawaii',
+            'Idaho',
+            'Illinois',
+            'Indiana',
+            'Iowa',
+            'Kansas',
+            'Kentucky',
+            'Louisiana',
+            'Maine',
+            'Marshall Islands',
+            'Maryland',
+            'Massachusetts',
+            'Michigan',
+            'Minnesota',
+            'Mississippi',
+            'Missouri',
+            'Montana',
+            'Nebraska',
+            'Nevada',
+            'New Hampshire',
+            'New Jersey',
+            'New Mexico',
+            'New York',
+            'North Carolina',
+            'North Dakota',
+            'Northern Mariana Islands',
+            'Ohio',
+            'Oklahoma',
+            'Oregon',
+            'Palau',
+            'Pennsylvania',
+            'Puerto Rico',
+            'Rhode Island',
+            'South Carolina',
+            'South Dakota',
+            'Tennessee',
+            'Texas',
+            'Utah',
+            'Vermont',
+            'Virgin Island',
+            'Virginia',
+            'Washington',
+            'West Virginia',
+            'Wisconsin',
+            'Wyoming',
+            ],
+        }
+    },
+    watch: {
+      search (val) {
+        val && val !== this.select && this.querySelections(val)
+      },
+    },
+    methods: {
+      querySelections (v) {
+        this.loading = true
+        setTimeout(() => {
+          this.items = this.districts.filter(e => {
+            return (e || '').toLowerCase().indexOf((v || '').toLowerCase()) > -1
+          })
+          this.loading = false
+        }, 500)
+      },
+      onScroll (e) {
+        this.offsetTop = e.target.scrollTop
+      },
+    }
+}
+</script>
